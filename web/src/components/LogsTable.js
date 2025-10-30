76463b540a1fac80eb4545483feddb5bbe5dc22c
liyunfei <2716499331@qq.com>
2025-10-30 17:36:20 +0800

    Hide channel

--- web/src/components/LogsTable.js
+++ web/src/components/LogsTable.js
@@ -614,12 +614,12 @@ const LogsTable = () => {
         //   value: content,
         // })
       }      
-      if (isAdminUser && (logs[i].type === 0 || logs[i].type === 2)) {
-        expandDataLocal.push({
-          key: t('渠道信息'),
-          value: `${logs[i].channel} - ${logs[i].channel_name || '[未知]'}`
-        });
-      }
+      // if (isAdminUser && (logs[i].type === 0 || logs[i].type === 2)) {
+      //   expandDataLocal.push({
+      //     key: t('渠道信息'),
+      //     value: `${logs[i].channel} - ${logs[i].channel_name || '[未知]'}`
+      //   });
+      // }
       if (other?.ws || other?.audio) {
         expandDataLocal.push({
           key: t('语音输入'),
